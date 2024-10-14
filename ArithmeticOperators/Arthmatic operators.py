53bc909dc50ba9e58a22834911feadc9c49f1325
mukeshan313 <kq00733@gmail.com>
2024-10-14 12:29:56 +0530

    Add files via upload

---
+++ ArithmeticOperators/Arthmatic operators.py
@@ -0,0 +1,16 @@
+ari=input("enter the arithimatic operator")
+a=10
+b=20
+if (ari=="sum" or ari=="+"):
+    print(a+b)
+
+elif(ari=="sub" or ari=="-"):
+    print(a-b)
+
+elif(ari=="mul" or ari=="*"):
+    print(a*b)
+    
+else:
+    print(a/b)
+    
+
